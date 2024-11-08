0d7965792d7beda5b837fc48ae97c3e95b918058
julia snegova <ssnnggvv@gmail.com>
2024-11-08 19:36:31 +0300

    refactor: add gsap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,48 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { MD_BREAK } from "../constants/media";
 
 const Hero: FC = () => {
-  return (
-    <section className="container">
-      <div>
-        <h2>iPhone 15 Pro</h2>
-      </div>
-    </section>
-  )
-}
+   const [videoSrc, setVideoSrc] = useState(
+      window.innerWidth < MD_BREAK ? smallHeroVideo : heroVideo
+   );
 
-export default Hero
+   const handleVideoSrcSet = () => {
+      if (window.innerWidth < MD_BREAK) {
+         setVideoSrc(smallHeroVideo);
+      } else {
+         setVideoSrc(heroVideo);
+      }
+   };
+
+   useEffect(() => {
+      window.addEventListener("resize", handleVideoSrcSet);
+
+      return () => {
+         window.removeEventListener("resize", handleVideoSrcSet);
+      };
+   }, []);
+
+   useGSAP(() => {
+      gsap.to("#hero__title", { opacity: 1 });
+   }, []);
+
+   return (
+      <section className="container hero">
+         <div>
+            <h2 id="hero__title" className="hero__title">
+               iPhone 15 Pro
+            </h2>
+            <div className="hero__video">
+               <video autoPlay muted playsInline={true} key={videoSrc}>
+                  <source src={videoSrc} type="video/mp4" />
+               </video>
+            </div>
+         </div>
+      </section>
+   );
+};
+
+export default Hero;
---
+++ src/constants/media.ts
@@ -0,0 +1,4 @@
+export const SM_BREAK: number = 375;
+export const MD_BREAK: number = 720;
+export const LG_BREAK: number = 992;
+export const XL_BREAK: number = 1920;
