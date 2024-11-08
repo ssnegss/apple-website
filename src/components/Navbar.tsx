d4e26fe37553f546e11d0031fa2693d2ceb6523b
julia snegova <ssnnggvv@gmail.com>
2024-11-08 15:13:58 +0300

    refactor: add fontello

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,25 @@ import { appleImg } from "../utils";
 import { NavItem } from "../types/NavItem";
 
 const Navbar: FC = (): ReactNode => {
-  const navItems: Array<NavItem> = [
-    {
-       title: "Phones",
-       link: "#",
-    },
-    {
-       title: "Macbooks",
-       link: "#",
-    },
-    {
-       title: "Tablets",
-       link: "#",
-    },
- ];
+   const navItems: Array<NavItem> = [
+      {
+         title: "Phones",
+         link: "#",
+      },
+      {
+         title: "Macbooks",
+         link: "#",
+      },
+      {
+         title: "Tablets",
+         link: "#",
+      },
+   ];
 
- 
    return (
       <header>
          <nav>
-            <img src={appleImg} alt="Apple" />
+            <i className="icon-apple"></i>
             {navItems.map((navItem: NavItem, index) => (
                <div key={index}>{navItem.title}</div>
             ))}
