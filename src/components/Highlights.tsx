0ef8926ca9690d2f2a0aaf589b553aa1d15751ae
julia snegova <ssnnggvv@gmail.com>
2024-11-10 15:49:24 +0300

    refactor: add transitions

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -6,13 +6,19 @@ import { rightImg, watchImg } from "../utils";
 const Highlights: FC = () => {
    useGSAP(() => {
       gsap.to("#highlights__title", { opacity: 1 });
+      gsap.to(".highlights__watchBtn", {
+         opacity: 1,
+         duration: 1,
+         y: 0,
+         stagger: 0.25,
+      });
    }, []);
 
    return (
       <section id="highlights" className="container highlights">
          <div className="highlights__title-block">
             <h2 className="h2 highlights__title" id="highlights__title">
-               Get the highlights
+               Get the highlights.
             </h2>
             <div className="highlights__watchBtns">
                <div className="highlights__watchBtn">
