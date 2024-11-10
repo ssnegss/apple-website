258375901be7cfed6436bacb082602e1d1334785
julia snegova <ssnnggvv@gmail.com>
2024-11-10 15:02:07 +0300

    refactor: add gsap to highlights title

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,12 +1,20 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { FC } from "react";
 
 const Highlights: FC = () => {
+   useGSAP(() => {
+      gsap.to("#highlights__title", { opacity: 1 });
+   }, []);
+
    return (
       <section id="highlights" className="container highlights">
          <div>
-          <div>
-            <h2>Get the highlights</h2>
-          </div>
+            <div>
+               <h2 className="h2 highlights__title" id="highlights__title">
+                  Get the highlights
+               </h2>
+            </div>
          </div>
       </section>
    );
