9ab440faf6e73c52435af77d5fe668ecee539a4f
julia snegova <ssnnggvv@gmail.com>
2024-11-10 15:34:50 +0300

    add: add highlights title block

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { FC } from "react";
+import { rightImg, watchImg } from "../utils";
 
 const Highlights: FC = () => {
    useGSAP(() => {
@@ -9,11 +10,19 @@ const Highlights: FC = () => {
 
    return (
       <section id="highlights" className="container highlights">
-         <div>
-            <div>
-               <h2 className="h2 highlights__title" id="highlights__title">
-                  Get the highlights
-               </h2>
+         <div className="highlights__title-block">
+            <h2 className="h2 highlights__title" id="highlights__title">
+               Get the highlights
+            </h2>
+            <div className="highlights__watchBtns">
+               <div className="highlights__watchBtn">
+                  Watch the film
+                  <img src={watchImg} alt="watch" />
+               </div>
+               <div className="highlights__watchBtn">
+                  Watch the event
+                  <img src={rightImg} alt="right" />
+               </div>
             </div>
          </div>
       </section>
