70598d2344b42a7cbf79adee925cf5a03cc3faad
julia snegova <ssnnggvv@gmail.com>
2024-11-15 01:15:55 +0300

    set howItWorks text

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,8 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { chipImg, frameImg, frameVideo } from "../utils";
+import { useRef } from "react";
+import { animateWithGsap } from "../utils/animations";
 
 const HowItWorks = () => {
+   const videoRef = useRef(null);
+
    useGSAP(() => {
       gsap.from("#chip", {
          scrollTrigger: {
@@ -13,6 +17,13 @@ const HowItWorks = () => {
          scale: 2,
          duration: 2,
          ease: "power2.inOut",
+      });
+
+      animateWithGsap("#howItWorks__description", {
+         y: 0,
+         opacity: 1,
+         ease: "power2.inOut",
+         duration: 1,
       });
    }, []);
 
@@ -30,14 +41,42 @@ const HowItWorks = () => {
             <div className="howItWorks__subtitle">
                It's here. The biggest redesign in the history of Apple GPUs.
             </div>
-            <div className="howItWorks__videoFrameBlock">
-               <div className="howItWorks__videoFrame">
-                  <img src={frameImg} alt="frame" />
-                  <div className="howItWorks__video">
-                     <video>
-                        <source src={frameVideo} type="video/mp4" />
-                     </video>
+            <div className="howItWorks__videoFrame">
+               <img src={frameImg} alt="frame" />
+               <div className="howItWorks__video">
+                  <video
+                     playsInline
+                     preload="none"
+                     muted
+                     autoPlay
+                     ref={videoRef}
+                  >
+                     <source src={frameVideo} type="video/mp4" />
+                  </video>
+               </div>
+            </div>
+            <div className="howItWorks__videoDescription">
+               Honkai: Star Rail
+            </div>
+            <div
+               className="howItWorks__description"
+               id="howItWorks__description"
+            >
+               <div className="howItWorks__text">
+                  <div className="text">
+                     A17 Pro is an entirely new class of iPhone chip that
+                     delivers our <span>best graphic performance by far</span> .
                   </div>
+                  <div className="text">
+                     Mobile <span>games will look and feel so immersive</span>{" "}
+                     with incredibly detailed environments and characters.
+                  </div>
+               </div>
+
+               <div className="howItWorks__gpu">
+                  New
+                  <span>Pro-class GPU</span>
+                  with 6 cores
                </div>
             </div>
          </div>
